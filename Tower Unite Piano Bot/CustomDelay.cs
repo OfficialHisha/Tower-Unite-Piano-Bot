6819f8b4ef9486898a2eead3cf210082c875a8c9
Matias Jensen <sejemus@yahoo.dk>
2016-05-30 15:19:15 +0200

    Implemented custom delay options for ultimate customization

---
+++ Tower Unite Piano Bot/CustomDelay.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+namespace Tower_Unite_Piano_Bot
+{
+    class CustomDelay
+    {
+        public CheckBox EnabledBox { get; private set; }
+        public TextBox CharacterBox { get; private set; }
+        public NumericUpDown DelayBox { get; private set; }
+
+        public CustomDelay(CheckBox checkBox, TextBox textBox, NumericUpDown numberBox)
+        {
+            EnabledBox = checkBox;
+            CharacterBox = textBox;
+            DelayBox = numberBox;
+        }
+    }
+}
--- Tower Unite Piano Bot/Form1.cs
+++ Tower Unite Piano Bot/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -19,7 +20,6 @@ namespace Tower_Unite_Piano_Bot
         public string song = "{123456789QWERTYUIOPASDFGHJLZXCVBNM}";
         public int normalDelay = 200;
         public int fastDelay = 100;
-        public int longDelay = 200;
         public bool stop = true;
         public bool loop = false;
         Keys startKey;
@@ -30,6 +30,8 @@ namespace Tower_Unite_Piano_Bot
         OpenFileDialog loadFileDialog = new OpenFileDialog();
         SaveFileDialog saveFileDialog = new SaveFileDialog();
         #endregion
+        
+        List<CustomDelay> activeCustomDelays = new List<CustomDelay>();
 
         GlobalKeyboardHook gkh = new GlobalKeyboardHook();
 
@@ -58,7 +60,6 @@ namespace Tower_Unite_Piano_Bot
             stopKey = (Keys)keysConverter.ConvertFromString(StopKeyTextBox.Text.ToString());
             gkh.HookedKeys.Add(startKey);
             gkh.HookedKeys.Add(stopKey);
-            
         }
 
         private void PlayButton_Click(object sender, EventArgs e)
@@ -73,7 +74,6 @@ namespace Tower_Unite_Piano_Bot
             song = SongTextBox.Text;
             normalDelay = (int)NormalDelayBox.Value;
             fastDelay = (int)FastDelayBox.Value;
-            longDelay = (int)LongPauseDelayBox.Value;
 
             Process p;
             if ((string)ProcessChooserBox.SelectedItem == "GMT")
@@ -81,6 +81,7 @@ namespace Tower_Unite_Piano_Bot
             else
                 p = Process.GetProcessesByName("Tower").FirstOrDefault();
 
+            //p = Process.GetProcessesByName("notepad").FirstOrDefault();//For development testing.
             if (p != null)
             {
                 IntPtr h = p.MainWindowHandle;
@@ -248,22 +249,25 @@ namespace Tower_Unite_Piano_Bot
                 if (note == '\n' || note == '\r')
                     continue;
 
-                #region Pauses
-                //Long pause
-                if (note == '.')
-                {
-                    Thread.Sleep(longDelay);
-                    continue;
-                }
-                
+                #region Pauses                
                 //Normal pause
                 if (note == ' ')
                 {
                     Thread.Sleep(normalDelay);
                     continue;
                 }
-                #endregion
 
+                //Custom pauses
+                if(activeCustomDelays.Count != 0)
+                {
+                    CustomDelay customDelay = activeCustomDelays.Find(x => x.CharacterBox.Text.ToCharArray()[0] == note);
+                    if(customDelay != null)
+                    {
+                        Thread.Sleep((int)customDelay.DelayBox.Value);
+                        continue;
+                    }
+                }
+                #endregion
                 #region Multiple notes
                 if (isMultiPress)
                 {
@@ -343,5 +347,312 @@ namespace Tower_Unite_Piano_Bot
             }
             e.Handled = true;
         }
+
+        #region Custom delays
+        private void DelayCheckBox0_CheckedChanged(object sender, EventArgs e)
+        {
+            if(!DelayCheckBox0.Checked)
+            {
+                CustomDelayCharLabel0.Enabled = false;
+                DelayCharacter0.Enabled = false;
+                CustomDelayDelayLabel0.Enabled = false;
+                DelayTime0.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter0));
+            }
+            else
+            {
+                CustomDelayCharLabel0.Enabled = true;
+                DelayCharacter0.Enabled = true;
+                CustomDelayDelayLabel0.Enabled = true;
+                DelayTime0.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox0, DelayCharacter0, DelayTime0));
+            }
+        }
+        private void DelayCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox1.Checked)
+            {
+                CustomDelayCharLabel1.Enabled = false;
+                DelayCharacter1.Enabled = false;
+                CustomDelayDelayLabel1.Enabled = false;
+                DelayTime1.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter1));
+            }
+            else
+            {
+                CustomDelayCharLabel1.Enabled = true;
+                DelayCharacter1.Enabled = true;
+                CustomDelayDelayLabel1.Enabled = true;
+                DelayTime1.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox1, DelayCharacter1, DelayTime1));
+            }
+        }
+        private void DelayCheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox2.Checked)
+            {
+                CustomDelayCharLabel2.Enabled = false;
+                DelayCharacter2.Enabled = false;
+                CustomDelayDelayLabel2.Enabled = false;
+                DelayTime2.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter2));
+            }
+            else
+            {
+                CustomDelayCharLabel2.Enabled = true;
+                DelayCharacter2.Enabled = true;
+                CustomDelayDelayLabel2.Enabled = true;
+                DelayTime2.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox2, DelayCharacter2, DelayTime2));
+            }
+        }
+        private void DelayCheckBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox3.Checked)
+            {
+                CustomDelayCharLabel3.Enabled = false;
+                DelayCharacter3.Enabled = false;
+                CustomDelayDelayLabel3.Enabled = false;
+                DelayTime3.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter3));
+            }
+            else
+            {
+                CustomDelayCharLabel3.Enabled = true;
+                DelayCharacter3.Enabled = true;
+                CustomDelayDelayLabel3.Enabled = true;
+                DelayTime3.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox3, DelayCharacter3, DelayTime3));
+            }
+        }
+        private void DelayCheckBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox4.Checked)
+            {
+                CustomDelayCharLabel4.Enabled = false;
+                DelayCharacter4.Enabled = false;
+                CustomDelayDelayLabel4.Enabled = false;
+                DelayTime4.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter4));
+            }
+            else
+            {
+                CustomDelayCharLabel4.Enabled = true;
+                DelayCharacter4.Enabled = true;
+                CustomDelayDelayLabel4.Enabled = true;
+                DelayTime4.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox4, DelayCharacter4, DelayTime4));
+            }
+        }
+        private void DelayCheckBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox5.Checked)
+            {
+                CustomDelayCharLabel5.Enabled = false;
+                DelayCharacter5.Enabled = false;
+                CustomDelayDelayLabel5.Enabled = false;
+                DelayTime5.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter5));
+            }
+            else
+            {
+                CustomDelayCharLabel5.Enabled = true;
+                DelayCharacter5.Enabled = true;
+                CustomDelayDelayLabel5.Enabled = true;
+                DelayTime5.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox5, DelayCharacter5, DelayTime5));
+            }
+        }
+        private void DelayCheckBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox6.Checked)
+            {
+                CustomDelayCharLabel6.Enabled = false;
+                DelayCharacter6.Enabled = false;
+                CustomDelayDelayLabel6.Enabled = false;
+                DelayTime6.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter6));
+            }
+            else
+            {
+                CustomDelayCharLabel6.Enabled = true;
+                DelayCharacter6.Enabled = true;
+                CustomDelayDelayLabel6.Enabled = true;
+                DelayTime6.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox6, DelayCharacter6, DelayTime6));
+            }
+        }
+        private void DelayCheckBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox7.Checked)
+            {
+                CustomDelayCharLabel7.Enabled = false;
+                DelayCharacter7.Enabled = false;
+                CustomDelayDelayLabel7.Enabled = false;
+                DelayTime7.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter7));
+            }
+            else
+            {
+                CustomDelayCharLabel7.Enabled = true;
+                DelayCharacter7.Enabled = true;
+                CustomDelayDelayLabel7.Enabled = true;
+                DelayTime7.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox7, DelayCharacter7, DelayTime7));
+            }
+        }
+        private void DelayCheckBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox8.Checked)
+            {
+                CustomDelayCharLabel8.Enabled = false;
+                DelayCharacter8.Enabled = false;
+                CustomDelayDelayLabel8.Enabled = false;
+                DelayTime8.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter8));
+            }
+            else
+            {
+                CustomDelayCharLabel8.Enabled = true;
+                DelayCharacter8.Enabled = true;
+                CustomDelayDelayLabel8.Enabled = true;
+                DelayTime8.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox8, DelayCharacter8, DelayTime8));
+            }
+        }
+        private void DelayCheckBox9_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox9.Checked)
+            {
+                CustomDelayCharLabel9.Enabled = false;
+                DelayCharacter9.Enabled = false;
+                CustomDelayDelayLabel9.Enabled = false;
+                DelayTime9.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter9));
+            }
+            else
+            {
+                CustomDelayCharLabel9.Enabled = true;
+                DelayCharacter9.Enabled = true;
+                CustomDelayDelayLabel9.Enabled = true;
+                DelayTime9.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox9, DelayCharacter9, DelayTime9));
+            }
+        }
+        private void DelayCheckBox10_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox10.Checked)
+            {
+                CustomDelayCharLabel10.Enabled = false;
+                DelayCharacter10.Enabled = false;
+                CustomDelayDelayLabel10.Enabled = false;
+                DelayTime10.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter10));
+            }
+            else
+            {
+                CustomDelayCharLabel10.Enabled = true;
+                DelayCharacter10.Enabled = true;
+                CustomDelayDelayLabel10.Enabled = true;
+                DelayTime10.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox10, DelayCharacter10, DelayTime10));
+            }
+        }
+        private void DelayCheckBox11_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox11.Checked)
+            {
+                CustomDelayCharLabel11.Enabled = false;
+                DelayCharacter11.Enabled = false;
+                CustomDelayDelayLabel11.Enabled = false;
+                DelayTime11.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter11));
+            }
+            else
+            {
+                CustomDelayCharLabel11.Enabled = true;
+                DelayCharacter11.Enabled = true;
+                CustomDelayDelayLabel11.Enabled = true;
+                DelayTime11.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox11, DelayCharacter11, DelayTime11));
+            }
+        }
+        private void DelayCheckBox12_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox12.Checked)
+            {
+                CustomDelayCharLabel12.Enabled = false;
+                DelayCharacter12.Enabled = false;
+                CustomDelayDelayLabel12.Enabled = false;
+                DelayTime12.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter12));
+            }
+            else
+            {
+                CustomDelayCharLabel12.Enabled = true;
+                DelayCharacter12.Enabled = true;
+                CustomDelayDelayLabel12.Enabled = true;
+                DelayTime12.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox12, DelayCharacter12, DelayTime12));
+            }
+        }
+        private void DelayCheckBox13_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox13.Checked)
+            {
+                CustomDelayCharLabel13.Enabled = false;
+                DelayCharacter13.Enabled = false;
+                CustomDelayDelayLabel13.Enabled = false;
+                DelayTime13.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter13));
+            }
+            else
+            {
+                CustomDelayCharLabel13.Enabled = true;
+                DelayCharacter13.Enabled = true;
+                CustomDelayDelayLabel13.Enabled = true;
+                DelayTime13.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox13, DelayCharacter13, DelayTime13));
+            }
+        }
+        private void DelayCheckBox14_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox14.Checked)
+            {
+                CustomDelayCharLabel14.Enabled = false;
+                DelayCharacter14.Enabled = false;
+                CustomDelayDelayLabel14.Enabled = false;
+                DelayTime14.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter14));
+            }
+            else
+            {
+                CustomDelayCharLabel14.Enabled = true;
+                DelayCharacter14.Enabled = true;
+                CustomDelayDelayLabel14.Enabled = true;
+                DelayTime14.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox14, DelayCharacter14, DelayTime14));
+            }
+        }
+        private void DelayCheckBox15_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!DelayCheckBox15.Checked)
+            {
+                CustomDelayCharLabel15.Enabled = false;
+                DelayCharacter15.Enabled = false;
+                CustomDelayDelayLabel15.Enabled = false;
+                DelayTime15.Enabled = false;
+                activeCustomDelays.Remove(activeCustomDelays.Find(x => x.CharacterBox == DelayCharacter15));
+            }
+            else
+            {
+                CustomDelayCharLabel15.Enabled = true;
+                DelayCharacter15.Enabled = true;
+                CustomDelayDelayLabel15.Enabled = true;
+                DelayTime15.Enabled = true;
+                activeCustomDelays.Add(new CustomDelay(DelayCheckBox15, DelayCharacter15, DelayTime15));
+            }
+        }
+        #endregion
     }
 }
